3ec74cbb6e01fd06a3bd1aa636e41e593c7238db
OGEmanuel <emanuolumide@mail.com>
2023-05-27 00:38:51 +0100

    Connected FE to comments API Routes

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -6,6 +6,7 @@ import EventLogistics from '../../components/event-detail/event-logistics';
 import EventContent from '../../components/event-detail/event-content';
 import ErrorAlert from '../../components/ui/error-alert';
 import Head from 'next/head';
+import Comments from '@/components/input/comments';
 
 function EventDetailPage(props) {
   const { selectedEvent } = props;
@@ -26,16 +27,17 @@ function EventDetailPage(props) {
         <title>{event.title}</title>
         <meta name="description" content={event.description} />
       </Head>
-        <EventSummary title={event.title} />
-        <EventLogistics
-          date={event.date}
-          address={event.location}
-          image={event.image}
-          imageAlt={event.title}
-        />
-        <EventContent>
-          <p>{event.description}</p>
-        </EventContent>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+      <Comments eventId={event.id} />
     </Fragment>
   );
 }
