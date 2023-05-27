91ae256906283233b5faca3426c70ac35b830128
OGEmanuel <emanuolumide@mail.com>
2023-05-27 04:22:15 +0100

    added MongoDB

--- pages/api/comments/[eventId].js
+++ pages/api/comments/[eventId].js
@@ -1,5 +1,20 @@
-const handler = (req, res) => {
+import {
+  connectDatabase,
+  insertDocument,
+  getAllDocuments,
+} from '@/helpers/db-util';
+
+const handler = async (req, res) => {
   const eventId = req.query.eventId;
+
+  let client;
+
+  try {
+    client = await connectDatabase();
+  } catch (error) {
+    res.status(500).json({ message: 'Connecting to the database failed!' });
+    return;
+  }
 
   if (req.method === 'POST') {
     const { email, name, text } = req.body;
@@ -12,29 +27,39 @@ const handler = (req, res) => {
       text.trim() === ''
     ) {
       res.status(422).json({ message: 'Invalid input.' });
+      client.close();
       return;
     }
 
     const newComment = {
-      id: new Date().toISOString(),
       email,
       name,
       text,
+      eventId,
     };
 
-    console.log(newComment);
+    let result;
 
-    res.status(201).json({ message: 'Added comment.', comment: newComment });
+    try {
+      result = await insertDocument(client, 'comments', newComment);
+      newComment._id = result.insertedId;
+      res.status(201).json({ message: 'Added comment.', comment: newComment });
+    } catch (error) {
+      res.status(500).json({ message: 'Inserting comment failed!' });
+    }
   }
 
   if (req.method === 'GET') {
-    const dummyList = [
-      { id: 'c1', name: 'Max', text: 'A first comment' },
-      { id: 'c2', name: 'Manuel', text: 'A second comment' },
-    ];
-
-    res.status(200).json({ comments: dummyList });
+    let documents;
+    try {
+      documents = await getAllDocuments(client, 'comments', { _id: -1 });
+      res.status(200).json({ comments: documents });
+    } catch (error) {
+      res.status(500).json({ message: 'Getting comments failed.' });
+    }
   }
+
+  client.close();
 };
 
 export default handler;
